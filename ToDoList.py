857c136b8c407d87bdc51ec8f73bdb45e470c4a9
AmirAli Khoddam <85676002+9-AmirAli-9@users.noreply.github.com>
2024-07-30 19:35:39 +0330

    ToDoList.py

--- ToDoList.py
+++ ToDoList.py
@@ -32,28 +32,28 @@ class ToDoList:
         todoitem.pop(dnum)
         
 print("---to do list---")
-x=0
-while (x !='6'):
+choice=0
+while (choice !='6'):
     print('1. Add ToDo Item')
     print('2. Display ToDo List')
     print('3. Mark Item as Done')
     print('4. Edit ToDo Item')
     print('5. Delet ToDo Item')
     print('6. Exit')
-    x=input('Enter your choice: ')
+    choice=input('Enter your choice: ')
     
-    if x=='1' :
+    if choice=='1' :
         name=input('enter your item description: ')
         todolist1=ToDoList()
         todolist1.AddItem(name)
-    elif x=='2':
+    elif choice=='2':
         todolist1.Display()
-    elif x=='3':
+    elif choice=='3':
         todolist1.MarkAsDone()
-    elif x=='4':
+    elif choice=='4':
         todolist1.EditItem()
-    elif x=='5':
+    elif choice=='5':
         todolist1.DeletItem()
-    elif x=='6':
+    elif choice=='6':
         print('6')
 
